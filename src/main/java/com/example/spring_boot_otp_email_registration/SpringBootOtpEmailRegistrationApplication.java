abc86032f71ffb8bafbe5838d67fb55925267904
manoj <kannamanoj2@gmail.com>
2022-08-15 21:20:40 +0530

    Initial commit

---
+++ src/main/java/com/example/spring_boot_otp_email_registration/SpringBootOtpEmailRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_otp_email_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOtpEmailRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOtpEmailRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_otp_email_registration/SpringBootOtpEmailRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_otp_email_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOtpEmailRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
